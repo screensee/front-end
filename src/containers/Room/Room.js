69703e5a548fff89c71dad5ffab06bd991858d0a
Jurij Zakharchuk <yurastein@gmail.com>
2018-10-22 22:46:51 +0300

    fixed room join/exit

--- src/containers/Room/Room.js
+++ src/containers/Room/Room.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import queryString from 'query-string';
@@ -32,7 +32,7 @@ const UserSidebar = styled.div`
 
 const StyledUserIcon = styled(UserIcon)`
   margin-bottom: 15px;
-  
+
   &:last-child {
     margin-bottom: 0;
   }
@@ -51,18 +51,21 @@ const MoreUsers = styled.span`
 class Room extends PureComponent {
   static propTypes = {
     room: PropTypes.object,
-    id: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
   };
 
   static defaultProps = {
-    room: null,
+    room: null
   };
 
   static getDerivedStateFromProps(nextProps, prevState) {
     let result = null;
-    if (nextProps.room && _.get(nextProps.room, 'id') !== _.get(prevState.room, 'id')) {
+    if (
+      nextProps.room &&
+      _.get(nextProps.room, 'id') !== _.get(prevState.room, 'id')
+    ) {
       result = {
-        room: nextProps.room,
+        room: nextProps.room
       };
     }
     return result;
@@ -74,26 +77,25 @@ class Room extends PureComponent {
       videoId: '',
       room: null,
       messages: [],
+      participants: [],
     };
     this.sendMessage = makeRequest('post')(createUrl.messPost());
   }
 
   componentDidMount() {
-    window.addEventListener("beforeunload", this.exitRoom, false);
+    window.addEventListener('beforeunload', this.exitRoom, false);
 
     if (!this.props.room && this.props.id) {
-      makeRequest('post')(createUrl.roomJoin(this.props.id))()
-        .then((room) => {
-          this.setState({
-            room,
-          });
-          makeRequest('get')(createUrl.messGet(room.id))()
-            .then((messages) => {
-              this.setState({
-                messages,
-              });
-            })
+      makeRequest('post')(createUrl.roomJoin(this.props.id))().then(room => {
+        this.setState({
+          room
         });
+        makeRequest('get')(createUrl.messGet(room.id))().then(messages => {
+          this.setState({
+            messages
+          });
+        });
+      });
     } else if (this.props.room) {
       this.initRoom();
     }
@@ -107,7 +109,7 @@ class Room extends PureComponent {
 
   componentWillUnmount() {
     makeRequest('post')(createUrl.exitRoom(this.state.room.id))();
-    window.removeEventListener("beforeunload", this.exitRoom, false);
+    window.removeEventListener('beforeunload', this.exitRoom, false);
   }
 
   initRoom = () => {
@@ -119,39 +121,38 @@ class Room extends PureComponent {
     if (room.videoLink) {
       this.changeVideo(room.videoLink);
     }
-  }
+  };
 
-  onRemoteMessage = (message) => {
-    this.setState((state) => ({
-      messages: [
-        ...state.messages,
-        message,
-      ]
-    }))
+  onRemoteMessage = message => {
+    this.setState(state => ({
+      messages: [...state.messages, message]
+    }));
   };
 
   exitRoom = () => {
     makeRequest('post')(createUrl.exitRoom(this.state.room.id))();
   };
 
-  onSendMessage = (text) => {
+  onSendMessage = text => {
     this.sendMessage({
       roomId: this.state.room.id,
-      text,
-    }).then((message) => {
+      text
+    }).then(message => {
       // this.setState({
       //   messages,
       // })
     });
   };
 
-  onUserRoomAction = (isJoin) => (user) => {
-    this.setState((state) => {
+  onUserRoomAction = isJoin => user => {
+    this.setState(state => {
       let newParticipants;
       if (isJoin) {
-        newParticipants = [...state.room.participants, user];
+        if (!_.includes(state.room.participants, user)) {
+          newParticipants = [...state.room.participants, user];
+        }
       } else {
-        const index = _.findIndex(state.room.participants, user);
+        const index = state.room.participants.indexOf(user);
         if (index !== -1) {
           state.room.participants.splice(index, 1);
           newParticipants = [...state.room.participants];
@@ -160,19 +161,19 @@ class Room extends PureComponent {
       return {
         room: {
           ...state.room,
-          participatns: newParticipants,
+          participants: newParticipants !== undefined ? newParticipants : state.room.participants,
         }
-      }
+      };
     });
-  }
+  };
 
-  changeVideo = (value) => {
+  changeVideo = value => {
     let id;
     const splitted = value.split('?');
     if (splitted.length > 1) {
       const params = queryString.parse(splitted[1]);
       if (params.v) {
-        id = params.v
+        id = params.v;
       }
     } else {
       id = value;
@@ -185,31 +186,37 @@ class Room extends PureComponent {
     let toRender = null;
     if (this.state.room) {
       const { participants } = this.state.room;
-      toRender = participants.map((elem) => <StyledUserIcon name={elem} key={elem} img="" />);
+      toRender = participants.map(elem => (
+        <StyledUserIcon name={elem} key={elem} img="" />
+      ));
     }
     return toRender;
   };
 
   render() {
-    const { videoId, room, } = this.state;
+    const { videoId, room } = this.state;
     return (
       <RoomWrapper>
         <UserSidebar>
           {this.renderUserIcons()}
           {false && <MoreUsers>...</MoreUsers>}
         </UserSidebar>
-        <Chat onSendMessage={this.onSendMessage} messages={this.state.messages} />
-        <SettingsBar changeVideo={this.changeVideo} defaultUrl={_.get(room, 'videoLink')} />
-        {
-          videoId !== '' &&
+        <Chat
+          onSendMessage={this.onSendMessage}
+          messages={this.state.messages}
+        />
+        <SettingsBar
+          changeVideo={this.changeVideo}
+          defaultUrl={_.get(room, 'videoLink')}
+        />
+        {videoId !== '' && (
           // <Video title="vid" width="560" height="315"
           //                                     src={`https://www.youtube.com/embed/${this.state.videoId}?rel=0&amp;controls=0&amp;showinfo=0`}
           //                                     frameBorder="0"
           //                                     allow="autoplay; encrypted-media"
           //                                     allowFullScreen />
           <Video videoId={videoId} isMaster={_.get(room, 'isMaster', false)} />
-        }
-
+        )}
       </RoomWrapper>
     );
   }
